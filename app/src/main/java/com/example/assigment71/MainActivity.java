e2dcc59ec2b679dc65b33c8b761352926f0e8b7d
OriOfek <aa6055@bs.amalnet.k12.il>
2020-09-03 00:00:23 +0300

    all without credits

--- app/src/main/java/com/example/assigment71/MainActivity.java
+++ app/src/main/java/com/example/assigment71/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.assigment71;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     //variables delecration
     String expression;
     EditText display;
+    boolean isLegal;
+    float lastSummery;
+    boolean summeryFlag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         //put values in the variables
         expression = "";
+        summeryFlag = false;
         display = (EditText) findViewById(R.id.disply);
     }
 
@@ -29,15 +34,18 @@ public class MainActivity extends AppCompatActivity {
     change the variable "expression" and add the right sign
     */
     public void add(View view) {
-        //get the text in the edit text and add to it '+'
-        expression = display.getText().toString();
-        expression += '+';
+        if (!(display.getText().toString().equals("ileagel expression") || display.getText().toString().equals("")))
+        {
+            //get the text in the edit text and add to it '+'
+            expression = display.getText().toString();
+            expression += '+';
 
-        //put the new text in the edit test
-        display.setText(expression);
+            //put the new text in the edit test
+            display.setText(expression);
 
-        //put the cursor in the end of the string
-        display.setSelection(display.getText().length());
+            //put the cursor in the end of the string
+            display.setSelection(display.getText().length());
+        }
     }
 
     /*
@@ -48,15 +56,18 @@ public class MainActivity extends AppCompatActivity {
     change the variable "expression" and add the right sign
     */
     public void sub(View view) {
-        //get the text in the edit text and add to it '-'
-        expression = display.getText().toString();
-        expression += '-';
+        if (!(display.getText().toString().equals("ileagel expression") || display.getText().toString().equals("")))
+        {
+            //get the text in the edit text and add to it '-'
+            expression = display.getText().toString();
+            expression += '-';
 
-        //put the new text in the edit test
-        display.setText(expression);
+            //put the new text in the edit test
+            display.setText(expression);
 
-        //put the cursor in the end of the string
-        display.setSelection(display.getText().length());
+            //put the cursor in the end of the string
+            display.setSelection(display.getText().length());
+        }
     }
 
     /*
@@ -67,15 +78,18 @@ public class MainActivity extends AppCompatActivity {
    change the variable "expression" and add the right sign
    */
     public void multiply(View view) {
-        //get the text in the edit text and add to it '*'
-        expression = display.getText().toString();
-        expression += '*';
+        if (!(display.getText().toString().equals("ileagel expression") || display.getText().toString().equals("")))
+        {
+            //get the text in the edit text and add to it '*'
+            expression = display.getText().toString();
+            expression += '*';
 
-        //put the new text in the edit test
-        display.setText(expression);
+            //put the new text in the edit test
+            display.setText(expression);
 
-        //put the cursor in the end of the string
-        display.setSelection(display.getText().length());
+            //put the cursor in the end of the string
+            display.setSelection(display.getText().length());
+        }
     }
 
     /*
@@ -86,15 +100,18 @@ public class MainActivity extends AppCompatActivity {
    change the variable "expression" and add the right sign
    */
     public void divide(View view) {
-        //get the text in the edit text and add to it '/'
-        expression = display.getText().toString();
-        expression += '/';
+        if (!(display.getText().toString().equals("ileagel expression") || display.getText().toString().equals("")))
+        {
+            //get the text in the edit text and add to it '/'
+            expression = display.getText().toString();
+            expression += '/';
 
-        //put the new text in the edit test
-        display.setText(expression);
+            //put the new text in the edit test
+            display.setText(expression);
 
-        //put the cursor in the end of the string
-        display.setSelection(display.getText().length());
+            //put the cursor in the end of the string
+            display.setSelection(display.getText().length());
+        }
     }
 
     /*
@@ -126,19 +143,28 @@ public class MainActivity extends AppCompatActivity {
         //in order to get the summery I need these variables
         String number = "";
         float summery = 0;
-        boolean isLegal = true;
+
+        // update the expression data and the condition of the expression
+        expression = display.getText().toString();
+        isLegal = true;
 
         // scan the string and get the summery
         for (int i = 0; i < expression.length(); i++) {
             if (expression.charAt(i) >= '0' && expression.charAt(i) <= '9') {
                 number += expression.charAt(i);
-            } else {
+            }
+            else {
                 summery = calculate(summery, sign, number);
+                number = "";
                 sign = expression.charAt(i);
             }
         }
+        summery = calculate(summery, sign, number);
+
         if (isLegal) {
             display.setText(String.valueOf(summery));
+            summeryFlag = true;
+            lastSummery = summery;
         }
         else {
             display.setText("ileagel expression");
@@ -146,36 +172,53 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*
+    This function prints "calculate".
+    input:the current summery the sign and the String value of the number
+    output:the updated value of the summmery
+    description:
+    this function does the actions on the numbers
+    */
     public float calculate(float summery, char sign, String number) {
         final char ADD = '+';
         final char SUB = '-';
         final char DIVIDE = '/';
         final char MULTIPLY = '*';
 
-        boolean isLegal = true;
-
         // check the options
-        if (sign == ADD) {
+        if (sign == ADD)
+        {
             summery += Float.valueOf(number);
-            number = "";
-        } else if (sign == SUB) {
-            if (!number.equals("")) {
+        }
+        else if (sign == SUB)
+        {
+            if (!number.equals(""))
+            {
                 summery -= Float.valueOf(number);
-                number = "";
-            }
-        } else if (sign == MULTIPLY) {
-            summery *= Float.valueOf(number);
-            number = "";
-        } else if (sign == DIVIDE) {
-            if (Float.valueOf(number) == 0) {
-                isLegal = false;
-            } else {
-                summery -= Float.valueOf(number);
-                number = "";
             }
         }
-
+        else if (sign == MULTIPLY)
+        {
+            summery *= Float.valueOf(number);
+        }
+        else if (sign == DIVIDE)
+        {
+            if (Float.valueOf(number) == 0)
+            {
+                isLegal = false;
+            }
+            else
+            {
+                summery /= Float.valueOf(number);
+            }
+        }
+        return summery;
     }
 
-
+    public void goToCredits(View view) {
+        Intent si = new Intent(this,credits.class);
+        si.putExtra("lastSummery",lastSummery);
+        si.putExtra("summeryFlag",summeryFlag);
+        startActivity(si);
+    }
 }
